e6a8460deff13c3463beefa0ab1c43a2b590ebbd
juferomero <30669277+juferomero@users.noreply.github.com>
2018-04-28 21:12:57 +0000

    Cambio testFrontend/head.php
    Cambio en las rutas de las librerias

--- testFrontend/head.php
+++ testFrontend/head.php
@@ -1,13 +1,13 @@
 <meta charset="utf-8"/>
 <meta name="viewport" content="width=device-width, user-scalable=no, inicial-scale=1.0, maxium-scale=1.0, minumum-scale=1.0"/>
-<link rel="stylesheet" href="../frameworks/jquery-ui-themes-1.12.1/themes/base/jquery-ui.css">
-<link rel="stylesheet" href="../frameworks/bootstrap-3.3.7-dist/css/bootstrap.min.css"/>
-<link rel="stylesheet" href="../frameworks/font-awesome-4.7.0/css/font-awesome.min.css"/>
-<script src="../frameworks/jquery-ui-1.12.1/jquery-ui.js"></script>
-<script src="../frameworks/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
-<script src="../highcharts/highcharts.js"></script>
-<script src="../highcharts/exporting.js"></script>
-<script src="../highcharts/data.js"></script>
+<link rel="stylesheet" href="frameworks/jquery-ui-themes-1.12.1/themes/base/jquery-ui.css">
+<link rel="stylesheet" href="frameworks/bootstrap-3.3.7-dist/css/bootstrap.min.css"/>
+<link rel="stylesheet" href="frameworks/font-awesome-4.7.0/css/font-awesome.min.css"/>
+<script src="frameworks/jquery-ui-1.12.1/jquery-ui.js"></script>
+<script src="frameworks/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+<script src="highcharts/highcharts.js"></script>
+<script src="highcharts/exporting.js"></script>
+<script src="highcharts/data.js"></script>
 <title>Aerolinea-Escalas <?php echo $titulo;?></title>
 <style>
     .sidebar-search
