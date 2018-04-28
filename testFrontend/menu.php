739867b7b5f4a365b3c62b1fbcff9a4743f9acbc
juferomero <30669277+juferomero@users.noreply.github.com>
2018-04-28 20:57:34 +0000

    Cambio
    Se modifico conexion, estilo de los div de los select y se agrego la variable a la llamada de la funcion xajax Buscar.

--- testFrontend/menu.php
+++ testFrontend/menu.php
@@ -1,7 +1,7 @@
         <div class="col-lg-12" style="padding-left: 10%;color: white;background-color: #0073ea;font-size: 20px">
           <h1 class="page-header">Aerolinea-Escalas <?php echo $titulo;?></h1>
         </div>
-        <div class="col-lg-2" style="padding-left: 0px;padding-top: 5%">
+        <div class="col-md-2" style="padding-left: 0px;padding-top: 5%">
           <div class="panel panel-success" style="margin-top:7px; max-height: 600px; overflow: auto">
             <div class="panel-body">
               <!--<a href="index.php">
@@ -16,14 +16,14 @@
                 </div>
               </a>-->
               <?php
-                $host = "172.10.7.92";
-                $db = "prueba_julian";
-                $user = "Architect05";
-                $pw = "P@ss0102";
+                $host = "sql205.260mb.net";
+                $db = "n260m_22004511_prueba_julian";
+                $user = "n260m_22004511";
+                $pw = "12345678";
                 $con = mysql_connect($host,$user,$pw)or die("Problema al conectar con el servidor");
                 mysql_set_charset('utf8', $con);
                 mysql_select_db($db,$con)or die("Problema al ingresar a la base de datos");
-                $sql = mysql_query("SELECT * FROM prueba_julian.ciudades");
+                $sql = mysql_query("SELECT * FROM ciudades");
                 $salida = "";
                 while ($row = mysql_fetch_array($sql))
                 {
@@ -36,8 +36,8 @@
                 <div class="col-md-8 bg-primary" style="border-radius:5px;width:80%;height: 30px;margin-left: 15%;margin-rigth: 15%">
                   <label class="col-md-3" style="margin-top: 5px">Origen</label>
                 </div>
-                <div class="col-md-8" style="border-radius:5px;width:100%;height: 30px;margin-left: 15%;margin-top: 5%">
-                  <select class="col-md-8" id="cdd_org" name="cdd_org" onchange="xajax_Buscar(1,xajax.getFormValues('formulario'))">
+                <div class="col-md-12" style="border-radius:5px;width:120%;height: 30px;margin-left: -10%;margin-top: 5%">
+                  <select class="col-md-12" id="cdd_org" name="cdd_org" onchange="xajax_Buscar(1,xajax.getFormValues('formulario'),0)">
                     <option value="">Seleccione...</option>
                     <?php
                     echo $salida;
@@ -48,8 +48,8 @@
                 <div class="col-md-8 bg-primary" style="border-radius:5px;width:80%;height: 30px;margin-left: 15%;margin-rigth: 15%">
                   <label class="col-md-3" style="margin-top: 5px">Destino</label>
                 </div>
-                <div class="col-md-8" style="border-radius:5px;width:100%;height: 30px;margin-left: 15%;margin-top: 5%">
-                  <select class="col-md-8" id="cdd_dest" name="cdd_dest" onchange="xajax_Buscar(1,xajax.getFormValues('formulario'))">
+                <div class="col-md-12" style="border-radius:5px;width:120%;height: 30px;margin-left: -10%;margin-top: 5%">
+                  <select class="col-md-12" id="cdd_dest" name="cdd_dest" onchange="xajax_Buscar(1,xajax.getFormValues('formulario'),0)">
                     <option value="">Seleccione...</option>
                     <?php
                     echo $salida;
